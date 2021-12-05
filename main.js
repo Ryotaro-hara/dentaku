6fb6ce39f8b33036f153d859d4c59eb1968a7470
EC2 Default User <ec2-user@ip-172-31-2-88.ec2.internal>
2021-12-05 01:20:08 +0000

    dentaku-firstcommit

---
+++ main.js
@@ -0,0 +1,36 @@
+ let result = document.getElementById("result");
+
+function equal(){
+  result.innerHTML = eval(result.innerHTML);
+}
+
+function reset() {
+ result.innerHTML = "0";
+}
+
+function clickbutton(target){
+  let input = target.innerHTML;
+  if(result.innerHTML == "0"){
+     result.innerHTML = input;
+  }else{
+   result.innerHTML += input;
+  }
+}
+
+function edit(target){
+   let input = target.innerHTML;
+
+   if(result.innerHTML.slice(-1)=="-"){
+      return;
+   }else if(result.innerHTML.slice(-1)=="+"){
+      return;
+   }else if(result.innerHTML.slice(-1)=="*"){
+      return;
+   }else if(result.innerHTML.slice(-1)=="/"){
+      return;
+   }else if(result.innerHTML.slice(-1)=="."){
+      return;
+   }else{
+      result.innerHTML += input;
+   }
+}
